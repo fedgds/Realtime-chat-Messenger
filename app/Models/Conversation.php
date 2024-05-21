de5a2d825ed323a29e334602c9cfa1297401f881
sangla3 <lavansang1264@gmail.com>
2024-05-21 11:21:33 +0700

    make model and migration

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'receiver_id',
+        'sender_id'
+    ];
+
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+
+    public function getReceiver()
+    {
+
+        if ($this->sender_id === auth()->id()) {
+
+            return User::firstWhere('id',$this->receiver_id);
+
+        } else {
+
+            return User::firstWhere('id',$this->sender_id);
+        }
+        
+
+    }
 }
